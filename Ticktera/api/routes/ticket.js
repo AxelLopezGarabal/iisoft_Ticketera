a69413734b7b1d014ffdb3560e763b50b59adef9
AxelLopezGarabal <axel.lopez.garabal@gmail.com>
2019-05-27 22:26:17 -0300

    se agrega la retrospectiva del sprint1 y se empieza el sprint2

--- Ticktera/api/routes/ticket.js
+++ Ticktera/api/routes/ticket.js
@@ -15,16 +15,16 @@ router.get('/', (req, res, next) => {
 
 router.post('/', (req, res, next) => {
 	const receptor = req.body.to;
-//	if(system.existEmplayeeWithName(receptor)){
-//		res.status(404).json({
-//			message: 'there is no employee whith name' + receptor + ''
-//		})
-//	}
+	if(system.existEmplayeeWithName(receptor)){
+		res.status(404).json({
+			message: 'there is no employee whith name' + receptor + ''
+		})
+	}
 	const newTicket = new ticketModule.Ticket(0, req.body.from, req.body.to, 
 		req.body.topic, req.body.content, req.body.state, req.body.priority);
 	res.status(200).json({
 		message: 'POST method',
-		createdTicket: JSON.stringify(newTicket)
+		createdTicket: newTicket
 	})
 });
 
